4cd87c7a515c734adc40886db3623d6ecc63aac5
K-Jung-H <jay5136@naver.com>
2024-05-26 19:05:50 +0900

    update_ api_1
    test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import http.client
 import urllib.parse
+import xml.etree.ElementTree as ET
 
 # API 키
 api_key = 'b109a81aed5742bab617df81af4feb9e'  # 여기에 발급받은 API 키를 넣으세요
@@ -13,7 +14,7 @@ params = {
     'Type': 'xml',   # XML 형식으로 데이터 요청
     'pIndex': 1,     # 페이지 번호
     'pSize': 100,    # 페이지 당 요청 숫자
-    'SIGUN_NM': '시흥시'  # 선택적으로 시군명 입력
+    'SIGUN_NM': '안산시'  # 선택적으로 시군명 입력
 }
 
 # 매개변수를 URL에 맞게 인코딩
@@ -31,7 +32,14 @@ response = conn.getresponse()
 print(response.status)
 if response.status == 200:
     response_data = response.read()
-    print(response_data.decode('utf-8'))
+    root = ET.fromstring(response_data)  # XML 파싱 후 root 정의
+    place_direction = root.find('.//REGION_DIV_NM').text
+    mntn_info = root.find('.//MNTN_INFO').text.split(', ')  # MNTN_INFO 요소의 내용을 리스트로 분할하여 mntn_info에 저장
+    local_MT_num = int(root.find('.//MNTN_CNT').text)  # MNTN_CNT 요소의 내용을 정수형으로 변환하여 저장
+    print("지역의 위치: 경기도 " + place_direction)
+    print("산의 개수:", local_MT_num)  # 결과 출력
+    print("산의 종류:", mntn_info)  # 결과 출력
+
 else:
     print("OpenAPI 요청이 실패했습니다! 다시 시도해주세요.")
 
