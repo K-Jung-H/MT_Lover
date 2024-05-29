19b3e7251406029034b8c78d1295795b049e64df
dlarlwns135 <dlarlwns135@naver.com>
2024-05-29 22:07:59 +0900

    test

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
 import xml_read
+import Gmap_Test
 
 MT_data = []
 
-class Application(tk.Tk):
+class Application:
     def __init__(self):
-        super().__init__()
-        self.title("등산돌이")
-        self.geometry("800x700")
+        self.window = tk.Tk()  # Tk 인스턴스 생성
+        self.window.title("등산돌이")
+        self.window.geometry("800x700")
 
         # Creating and placing the top entry with a search button
-        self.entry = tk.Entry(self, width=20, font=('Helvetica', 16))
+        self.entry = tk.Entry(self.window, width=20, font=('Helvetica', 16))
         self.entry.grid(row=0, column=0, padx=(20,0), pady=40, ipady=8, sticky='ew')
 
-        self.search_button = tk.Button(self, text="검색", font=('Helvetica', 16), command=self.SIGUN_search)
+        self.search_button = tk.Button(self.window, text="검색", font=('Helvetica', 16), command=self.SIGUN_search)
         self.search_button.grid(row=0, column=1, padx=(0,20), pady=40, sticky='w')
 
         # Apply custom style for Notebook tabs
@@ -25,7 +26,7 @@ class Application(tk.Tk):
         style.map('TNotebook.Tab', background=[('selected', 'lightblue')], foreground=[('selected', 'blue')])
 
         # Creating notebook with tabs
-        self.notebook = ttk.Notebook(self, style='TNotebook')
+        self.notebook = ttk.Notebook(self.window, style='TNotebook')
         self.notebook.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky='nsew')
 
         # Creating tabs
@@ -42,16 +43,16 @@ class Application(tk.Tk):
         self.listbox2 = self.create_tab_content(self.tab2)
         self.listbox3 = self.create_tab_content(self.tab3)
 
-        # # Creating and placing the top right image placeholder
-        self.image_weather = tk.Frame(self, bg='lightblue', width=260, height=120)
+        # Creating and placing the top right image placeholder
+        self.image_weather = tk.Frame(self.window, bg='lightblue', width=260, height=120)
         self.image_weather.grid(row=0, column=2, padx=50, pady=30, sticky='n')
 
         # Configuring grid weights to ensure proper resizing behavior
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=2)
-        self.grid_columnconfigure(2, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
+        self.window.grid_columnconfigure(1, weight=2)
+        self.window.grid_columnconfigure(2, weight=1)
+        self.window.grid_rowconfigure(1, weight=1)
+        self.window.grid_rowconfigure(2, weight=1)
 
     def create_tab_content(self, tab):
         def on_item_select(event):
@@ -82,11 +83,6 @@ class Application(tk.Tk):
                 text_area.insert(tk.END, "산 관리 주체 : " + data["MT_ADMIN"] + '\n')
                 text_area.insert(tk.END, "산 관리 주체 연락처 : " + data["MT_ADMIN_NUM"] + '\n')
 
-            # 선택된 항목에 대한 처리
-            # print("선택된 항목:", selected_item)
-            #
-            # xml_read.MT_data_read()
-
         # Label 위젯을 추가하여 특정 텍스트 출력
         labelText = " "
         if tab == self.tab1:
@@ -96,7 +92,7 @@ class Application(tk.Tk):
         elif tab == self.tab3:
             labelText = "Tab 3에 표시할 다른 특정 텍스트"
 
-        label = tk.Label(tab, text=labelText, font=('Helvetica', 12), bg='white', fg='black')  # fg를 추가하여 텍스트 색상 설정
+        label = tk.Label(tab, text=labelText, font=('Helvetica', 12), bg='white', fg='black')
         label.grid(row=0, column=0, padx=20, pady=20, sticky='n')
 
         # Listbox 설정
@@ -110,8 +106,8 @@ class Application(tk.Tk):
         # Scrollbar for the listbox
         scrollbar = tk.Scrollbar(listbox_frame, orient=tk.VERTICAL)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        scrollbar.config(command=listbox.yview)  # listbox와 scrollbar 연결
-        listbox.config(yscrollcommand=scrollbar.set)  # scrollbar와 listbox 연결
+        scrollbar.config(command=listbox.yview)
+        listbox.config(yscrollcommand=scrollbar.set)
 
         # 텍스트 출력 프레임 설정
         text_frame = tk.Frame(tab, bg='white', height=100)
@@ -120,8 +116,11 @@ class Application(tk.Tk):
         text_area = tk.Text(text_frame)
         text_area.pack(fill='both', expand=True)
 
-        graph_frame = tk.Canvas(tab, bg='white', height=200)
-        graph_frame.grid(row=3, column=0, sticky='sew', padx=20, pady=20)
+        # graph_frame = tk.Frame(tab, bg='white', height=50)
+        # graph_frame.grid(row=3, column=0, sticky='sew', padx=20, pady=20)
+        #
+        # graph_area = tk.Canvas(graph_frame)
+        # graph_area.pack()
 
         # 이미지 프레임 설정
         image_frame = tk.Frame(tab, bg='lightgreen', width=600)
@@ -135,24 +134,26 @@ class Application(tk.Tk):
         tab.grid_rowconfigure(2, weight=1)
         tab.grid_rowconfigure(3, weight=1)
 
-        return listbox  # 수정된 부분: listbox를 반환합니다.
+        return listbox
 
     def SIGUN_search(self):
         global MT_data
-        self.listbox1.delete(0, tk.END)  # 탭 1 리스트 상자 초기화
+        self.listbox1.delete(0, tk.END)
         search_text = self.entry.get()
         print("검색어:", search_text)
-        MT_data = []  # MT_data 초기화
+        MT_data = []
         mountain_data = xml_read.MT_data_read(search_text)
         if mountain_data is not None:
             MT_data.append(mountain_data)
             for mountain_data in MT_data:
                 for mountain_name in mountain_data["mntn_info"]:
                     self.listbox1.insert(tk.END, mountain_name)
+        Gmap_Test.print_map(search_text)
 
-
-
+    def run(self):
+        self.window.mainloop()
 
 if __name__ == "__main__":
     app = Application()
-    app.mainloop()
+    app.run()
+
