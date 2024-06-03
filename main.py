0a5e8e339d95065f20a234918229cefccdbd5621
K-Jung-H <jay5136@naver.com>
2024-06-03 23:45:50 +0900

    add_mapzip road

--- main.py
+++ main.py
@@ -1,69 +1,198 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
+import xml_read
+import Gmap_Test
 
-""" asda """
-class Application(tk.Tk):
+class Application:
     def __init__(self):
-        super().__init__()
-        self.title("Tkinter Layout with Tabs Example")
-        self.geometry("800x600")
+        self.window = tk.Tk()
+        self.window.title("등산돌이")
+        self.window.geometry("800x700")
+        self.local_mt_list = []
+        self.mt_info = []
+        self.create_widgets()
+        self.configure_grid()
 
-        # Creating and placing the top entry with a search button
-        self.entry = tk.Entry(self)
-        self.entry.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
+    def create_widgets(self):
+        # Entry and search button
+        self.entry = tk.Entry(self.window, width=20, font=('Helvetica', 16))
+        self.entry.grid(row=0, column=0, padx=(20,0), pady=40, ipady=8, sticky='ew')
 
-        self.search_button = tk.Button(self, text="Search")
-        self.search_button.grid(row=0, column=1, padx=5, pady=5)
+        self.search_button = tk.Button(self.window, text="검색", font=('Helvetica', 16), command=self.SIGUN_search)
+        self.search_button.grid(row=0, column=1, padx=(0,20), pady=40, sticky='w')
 
-        # Creating notebook with tabs
-        self.notebook = ttk.Notebook(self)
-        self.notebook.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
+        # Notebook with tabs
+        style = ttk.Style()
+        style.configure('TNotebook.Tab', padding=[40, 8], font=('Helvetica', 12), bg='black')
+        style.map('TNotebook.Tab', background=[('selected', 'lightblue')], foreground=[('selected', 'blue')])
 
-        # Creating tabs
+        self.notebook = ttk.Notebook(self.window, style='TNotebook')
+        self.notebook.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky='nsew')
+
         self.tab1 = tk.Frame(self.notebook, bg='white')
         self.tab2 = tk.Frame(self.notebook, bg='white')
         self.tab3 = tk.Frame(self.notebook, bg='white')
 
-        self.notebook.add(self.tab1, text="Tab 1")
-        self.notebook.add(self.tab2, text="Tab 2")
+        self.notebook.add(self.tab1, text="해당 지역의 산")
+        self.notebook.add(self.tab2, text="산의 높이 막대 그래프")
         self.notebook.add(self.tab3, text="Tab 3")
 
-        # Adding listbox and text areas to each tab
-        self.create_tab_content(self.tab1)
-        self.create_tab_content(self.tab2)
-        self.create_tab_content(self.tab3)
+        self.listbox1 = self.create_tab_content(self.tab1)
+        self.listbox3 = self.create_tab_content(self.tab3)
 
-        # Creating and placing the top right image placeholder
-        self.image_frame_top = tk.Frame(self, bg='lightblue', width=200, height=100)
-        self.image_frame_top.grid(row=0, column=2, rowspan=2, padx=5, pady=5, sticky='nsew')
+        # Image placeholder
+        self.image_weather = tk.Frame(self.window, bg='lightblue', width=260, height=120)
+        self.image_weather.grid(row=0, column=2, padx=50, pady=30, sticky='n')
 
-        # Creating and placing the bottom right image placeholder
-        self.image_frame_bottom = tk.Frame(self, bg='lightgreen', width=200, height=200)
-        self.image_frame_bottom.grid(row=2, column=0, columnspan=3, padx=5, pady=5, sticky='nsew')
+        self.map_label = tk.Label(self.window)
 
-        # Configuring grid weights to ensure proper resizing behavior
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
+        # Canvas for bar chart
+        self.canvas = tk.Canvas(self.tab2, bg='white', width=600, height=400)
+        self.canvas.grid(row=0, column=0, padx=20, pady=20, sticky='n')
 
+        # Bind event to tab2 selection
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_selected)
+
+        # Create bar chart initially
+        self.create_bar_chart()
+
+    def configure_grid(self):
+        self.window.grid_columnconfigure(0, weight=1)
+        self.window.grid_columnconfigure(1, weight=2)
+        self.window.grid_columnconfigure(2, weight=1)
+        self.window.grid_rowconfigure(1, weight=1)
+        self.window.grid_rowconfigure(2, weight=1)
     def create_tab_content(self, tab):
-        # Listbox on the left side of the tab
+        def on_item_select(event):
+            selected_index = event.widget.curselection()
+            if selected_index:
+                selected_item = event.widget.get(selected_index)
+                data = xml_read.MT_deap_data(selected_item)
+                text_area.delete('1.0', tk.END)
+
+                if data == -1:
+                    text_area.insert(tk.END, "산 정보 없음\n")
+                elif data and "MT_ADMIN_NUM" in data:
+                    text_area.insert(tk.END, f"산 이름 : {data['MT_NAME']}\n")
+                    text_area.insert(tk.END, f"산 코드 : {data['MT_CODE']}\n")
+                    text_area.insert(tk.END, f"산 위치 : {data['MT_LOCATION']}\n")
+                    text_area.insert(tk.END, f"산 높이 : {data['MT_HIGH']}\n")
+                    text_area.insert(tk.END, f"산 관리 주체 : {data['MT_ADMIN']}\n")
+                    text_area.insert(tk.END, f"산 관리 주체 연락처 : {data['MT_ADMIN_NUM']}\n")
+
+                    img = Gmap_Test.print_map(data["MT_NAME"])
+                    self.show_map_image(img)
+
+        label_texts = {
+            self.tab1: "Tab 1에 표시할 특정 텍스트",
+            self.tab2: "Tab 2에 표시할 다른 특정 텍스트",
+            self.tab3: "Tab 3에 표시할 다른 특정 텍스트"
+        }
+        label = tk.Label(tab, text=label_texts.get(tab, " "), font=('Helvetica', 12), bg='white', fg='black')
+        label.grid(row=0, column=0, padx=20, pady=20, sticky='n')
+
         listbox_frame = tk.Frame(tab, bg='white')
-        listbox_frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
+        listbox_frame.grid(row=1, column=0, sticky='nsew', padx=20, pady=10)
 
         listbox = tk.Listbox(listbox_frame)
-        listbox.pack(fill='both', expand=True)
+        listbox.pack(side=tk.LEFT, fill='both', expand=True)
+        listbox.bind('<<ListboxSelect>>', on_item_select)
 
-        # Text area on the right side of the tab
-        text_frame = tk.Frame(tab, bg='white')
-        text_frame.pack(side='right', fill='both', expand=True, padx=5, pady=5)
+        scrollbar = tk.Scrollbar(listbox_frame, orient=tk.VERTICAL)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar.config(command=listbox.yview)
+        listbox.config(yscrollcommand=scrollbar.set)
+
+        text_frame = tk.Frame(tab, bg='white', height=100)
+        text_frame.grid(row=2, column=0, sticky='nsew', padx=20, pady=20)
 
         text_area = tk.Text(text_frame)
         text_area.pack(fill='both', expand=True)
 
+        self.image_frame = tk.Frame(tab, bg='white', width=600, height=100)
+        self.image_frame.grid(row=0, column=1, rowspan=4, sticky='nsew', padx=20, pady=20)
 
-# if __name__ == "__main__":
-#     app = Application()
-#     app.mainloop()
+        tab.grid_columnconfigure(0, weight=1)
+        tab.grid_columnconfigure(1, weight=1)
+        tab.grid_rowconfigure(0, weight=1)
+        tab.grid_rowconfigure(1, weight=1)
+        tab.grid_rowconfigure(2, weight=1)
+        tab.grid_rowconfigure(3, weight=1)
+
+        return listbox
+
+    def show_map_image(self, img):
+        tk_img = ImageTk.PhotoImage(img)
+        self.map_label.configure(image=tk_img)
+        self.map_label.image = tk_img
+        self.map_label.place(x=375, y=260)
+
+    def SIGUN_search(self):
+        global MT_data
+        self.listbox1.delete(0, tk.END)
+        search_text = self.entry.get()
+        print("검색어:", search_text)
+        MT_data = []
+        mountain_data = xml_read.MT_data_read(search_text)
+        if mountain_data:
+            MT_data.append(mountain_data)
+            for mountain in MT_data:
+                for mountain_name in mountain["mntn_info"]:
+                    self.listbox1.insert(tk.END, mountain_name)
+                    self.local_mt_list.append(mountain_name)
+        print('산 리스트 테스트: ', self.local_mt_list)
+
+    def create_bar_chart(self):
+        # Clear canvas before drawing
+        self.canvas.delete("all")
+
+        # Drawing parameters
+        bar_width = 20
+        max_height = 0
+        mt_info = []  # 막대 그래프를 그릴 산의 정보 리스트
+
+        for mt in self.local_mt_list:
+            data = xml_read.MT_deap_data(mt)
+            if data == -1:
+                continue
+            elif data and "MT_ADMIN_NUM" in data:
+                mt_info.append((mt, data['MT_HIGH']))
+                max_height = max(max_height, float(data['MT_HIGH']))  # 최대 높이 갱신
+
+        canvas_height = 400
+        canvas_width = 600
+        x_offset = 50
+        y_offset = 50
+
+        # Draw bars
+        for i, (mt_name, mt_height) in enumerate(mt_info):
+            x0 = x_offset + i * 50
+            y0 = canvas_height - y_offset
+            x1 = x0 + bar_width
+            y1 = y0 - (float(mt_height) / max_height) * (canvas_height - 2 * y_offset)
+            self.canvas.create_rectangle(x0, y0, x1, y1, fill='skyblue')
+
+            # Add mountain names
+            self.canvas.create_text((x0 + x1) / 2, y0 + 10, text=mt_name, anchor='n',
+                                    font=('Helvetica', 10))
+
+        # Draw axes
+        self.canvas.create_line(x_offset, canvas_height - y_offset, canvas_width - x_offset,
+                                canvas_height - y_offset, width=2)
+        self.canvas.create_line(x_offset, canvas_height - y_offset, x_offset, y_offset, width=2)
+        self.canvas.create_text(canvas_width / 2, canvas_height - y_offset + 40, text="산 이름",  font=('Helvetica', 12))
+        self.canvas.create_text(x_offset - 20, canvas_height / 2, text="높이", font=('Helvetica', 12), anchor='e', angle=90)
+
+    def on_tab_selected(self, event):
+        selected_tab = event.widget.select()
+        tab_text = event.widget.tab(selected_tab, "text")
+        if tab_text == "산의 높이 막대 그래프":
+            self.create_bar_chart()
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run()
---
+++ matzip.py
@@ -0,0 +1,75 @@
+import http.client
+import urllib.parse
+import xml.etree.ElementTree as ET
+
+
+class TastePlaceAPI:
+    def __init__(self):
+        self.api_key = '9011fc15298c4ac68dee0da375463fa2'
+        self.server = "openapi.gg.go.kr"
+
+    def get_taste_places(self, sigun_nm):
+        # 요청 매개변수 설정
+        params = {
+            'KEY': self.api_key,
+            'Type': 'xml',
+            'pIndex': 1,
+            'pSize': 5,
+            'SIGUN_NM': sigun_nm
+        }
+
+        # 매개변수를 URL에 맞게 인코딩
+        query_string = urllib.parse.urlencode(params)
+
+        # 전체 URL 생성
+        url = f"/PlaceThatDoATasteyFoodSt?{query_string}"
+
+        # 연결 설정 및 API 요청 보내기
+        conn = http.client.HTTPSConnection(self.server)
+        conn.request("GET", url)
+
+        # 응답 받기
+        response = conn.getresponse()
+
+        if response.status == 200:
+            response_data = response.read()
+            root = ET.fromstring(response_data)  # XML 파싱 후 root 정의
+
+            places = []
+
+            for row in root.findall('.//row'):
+                SIGUN_NM = row.find('SIGUN_NM').text
+                RESTRT_NM = row.find('RESTRT_NM').text
+                TASTFDPLC_TELNO = row.find('TASTFDPLC_TELNO').text
+                REPRSNT_FOOD_NM = row.find('REPRSNT_FOOD_NM').text
+                REFINE_ZIP_CD = row.find('REFINE_ZIP_CD').text
+                REFINE_ROADNM_ADDR = row.find('REFINE_ROADNM_ADDR').text
+                REFINE_LOTNO_ADDR = row.find('REFINE_LOTNO_ADDR').text
+                REFINE_WGS84_LAT = row.find('REFINE_WGS84_LAT').text
+                REFINE_WGS84_LOGT = row.find('REFINE_WGS84_LOGT').text
+
+                data = {
+                    "SIGUN_NM": SIGUN_NM,  # 시군명
+                    "RESTRT_NM": RESTRT_NM,  # 음식점 명
+                    "TASTFDPLC_TELNO": TASTFDPLC_TELNO,  # 맛집 전화번호
+                    "REPRSNT_FOOD_NM": REPRSNT_FOOD_NM,  # 대표 음식명
+                    "REFINE_ZIP_CD": REFINE_ZIP_CD,  # 소재지 우편번호
+                    "REFINE_ROADNM_ADDR": REFINE_ROADNM_ADDR,  # 소재지 도로명 주소
+                    "REFINE_LOTNO_ADDR": REFINE_LOTNO_ADDR,  # 소재지 지번 주소
+                    "REFINE_WGS84_LAT": REFINE_WGS84_LAT,  # 위도
+                    "REFINE_WGS84_LOGT": REFINE_WGS84_LOGT  # 경도
+                }
+                places.append(data)
+
+            # 연결 닫기
+            conn.close()
+            print(places)
+            return places
+
+        else:
+            conn.close()
+            raise Exception("OpenAPI 요청이 실패했습니다! 다시 시도해주세요.")
+
+
+api = TastePlaceAPI()
+api.get_taste_places('시흥시')
